587d7a293e773d1ab60f42b750e9720ea89a379b
Alexander Andreev <carzil@yandex.ru>
2018-06-12 16:21:09 +0300

    fix example handler

--- conf/handler.py
+++ conf/handler.py
@@ -2,7 +2,7 @@ class Handler:
     def __init__(self, ctx, client):
         self.ctx = ctx
         self.client = client
-        self.conn = self.ctx.connect("tcp://localhost:80", self.entry)
+        self.ctx.connect("tcp://localhost:80", self.entry)
 
     def entry(self, backend):
         self.backend = backend
