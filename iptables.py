a58a0309de704baba42e7873fa70dab66fa7d2f2
Smalyuk Arseny <smalukav@yandex.ru>
2018-09-09 01:14:01 +0300

    Add ipv6 to iptables update script

--- iptables.py
+++ iptables.py
@@ -1,14 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
-import shlex
+import json
+import os
 import subprocess
 import sys
-import os
+
+
+def modify_rule(is_ipv6, is_del, portcullis_host, portcullis_port, port, name):
+    prog = '/sbin/ip6tables' if is_ipv6 else '/sbin/iptables'
+    action_flag = '-D' if is_del else '-A'
+    cmd = [prog, '-t', 'nat', action_flag, 'PREROUTING', '-p', 'tcp',
+        '--dport', str(port), '-j', 'DNAT', '--to-destination', '{portcullis_host}:{portcullis_port}'
+        '-m', 'comment', '--comment', 'Portcullis port forward for service {name}'
+    ]
+
+    if is_del:
+        while subprocess.call(cmd) == 0:
+            pass
+    else:
+        subprocess.check_call(cmd)
 
 
 def main():
     if len(sys.argv) < 3:
-        print('Usage: {} add/del config.py'.format(sys.argv[0]))
+        print('Usage: {} add/del config.json'.format(sys.argv[0]))
         return
 
     action = sys.argv[1]
@@ -20,36 +35,13 @@ def main():
         raise RuntimeError('You must be root to change IPTables')
 
     config_file = sys.argv[2]
-    config_str = open(config_file).read()
-    config = {}
-    exec(config_str, config)
+    config = json.load(open(config_file))
 
-    name = config['name']
-    port = config['port']
-    backend_port = config['backend_port']
-    backend_host = config['backend_host']
+    for val in config['ports']:
+        modify_rule(False, action == 'del', config['portcullis_host'], val['portcullis_port'], val['port'], config['name'])
 
-    action_flag = {
-        'add': '-A',
-        'del': '-D',
-    }[action]
-
-    cmd_str = (
-        f'/sbin/iptables -t nat {action_flag} PREROUTING -p tcp '
-        f'--dport {port} -j DNAT --to-destination {backend_host}:{backend_port} '
-        f'-m comment --comment "Portcullis port forward for service {name}"'
-    )
-    cmd = shlex.split(cmd_str)
-
-    subprocess.check_call(cmd)
-
-    if action == 'del':
-        while True:
-            try:
-                subprocess.check_call(cmd)
-            except subprocess.CalledProcessError:
-                # probably all rules deleted, if any other exists
-                break
+    for val in config['ipv6ports']:
+        modify_rule(False, action == 'del', config['portcullis_ipv6host'], val['portcullis_port'], val['port'], config['name'])
 
 
 if __name__ == '__main__':
